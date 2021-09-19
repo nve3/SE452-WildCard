300bf744900684507295e4397a544e4331175e56
Mike Harris <mharr111@depaul.edu>
2021-09-19 10:47:56 -0500

    upload spring skeleton code

---
+++ demo/src/test/java/edu/depaul/cdm/se452project/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se452project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
